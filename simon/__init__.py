a233695320bbbfcba2eff0d52e7dff6861509cf3
Houjun Liu <houjun@jemoka.com>
2023-08-19 18:01:58 -0700

    bulk index

--- simon/__init__.py
+++ simon/__init__.py
@@ -3,7 +3,7 @@ from .store import Datastore
 from .start import create_context
 from .models import AgentContext
 from .models import ParsedDocument
-from .components.documents import index_document, parse_web, parse_text, parse_tika
+from .components.documents import index_document, parse_web, parse_text, parse_tika, bulk_index
 
 
 
