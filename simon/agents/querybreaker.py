6bb46a6e3b60aad10bb0bcd28cabe116680778ec
Houjun Liu <houjun@jemoka.com>
2023-08-22 13:41:07 -0700

    optimized breaker part 2

--- simon/agents/querybreaker.py
+++ simon/agents/querybreaker.py
@@ -14,51 +14,36 @@ from ..models import *
 
 TEMPLATE = """
 System:
-The knowledge base contains information about specific terms and general information. For instance, "my coworker Bob", "Bob's preferences for socks", "eigenvalues", and "last year's tax return" are all valid information in the knowledge base. "last year's tax return" is a valid entry in the knowledgebase while "an excel sheet for last year's tax return" is not. Pay attention to logical keywords.
+The knowledge base contains information about specific terms and general information. For instance, "my coworker Bob", "Bob's preferences for socks", "eigenvalues", and "last year's tax return" are all valid information in the knowledge base. "last year's tax return" is a valid entry in the knowledgebase while "an excel sheet for last year's tax return" is not.
 
-You will be provided a partial slice of the human's notes and thoughts; your job is to identify what the human is actually trying to do, and convert that to key terms. These terms terms should together clarify and point to the exact thing the human is trying to look for.
+You will be provided a partial slice of the human's notes and thoughts; your job is to identify what the human is actually trying to do, and convert that to a more genreal question or statement that uses only keywords that could be found in the knowledge base.
 
 Also, fix the user's spelling.
 
 Here are few examples of successful conversions:
-Input:
-eigenvalue
-```output
-eigenvalues
-```
----
-Input:
-people to visit in Bangkok
-```output
-people in Bangkok, Bangkok
-```
----
-Input:
-Zorbabs is an immense species of animals which causes many distructions.
-```output
-Zorbabs, about Zorbabs
-```
----
-Input:
-What is a celender
-```output
-definition of calendar 
-```
+- "What's an eigenvalue?" => "Eigenvalues, Eigenvalues definition"
+- "Tell me about Zorbabs" => "Zorbabs"
+- "What is a celender" => "calendar"
+- "I'm traveling to Singapore next week! What should I do?" => "Singapore, singapore activities"
+- "Who should I visit in Bangkok?" => "people I know in Bangkok, Bangkok visit"
 
-Provide your output, like the example above, in a comma seperated list of keywords that would appear in the knowledge base. You may break a query into at most 2 items.
+Provide your output in this format:
 
 ```output
-some broken queries
+New, shortened statement for the database:
+""your full, new question/statement here.""
 ```
 
 Begin!
+
 Human:
-Input:
+Here is the question for you to patch:
 {input}
 
 AI:
 ```output
-I am now going to break your big query into at most two broken queries:
+New, shortened statement for the database:
+""
 """
 
 class QueryPromptFormatter(StringPromptTemplate):
@@ -73,7 +58,7 @@ class QueryOutputParser(BaseOutputParser):
     def parse(self, str):
         str = str.strip("`").strip("'").strip('"').strip()
         
-        return str.strip("-").strip()
+        return str.strip()
 
 class QueryBreaker(object):
     def __init__(self, context, verbose=False):
@@ -95,6 +80,5 @@ class QueryBreaker(object):
         out =  self.__chain.predict(input=question,
                                     entities=entities)
         res =  self.__prompt.output_parser.parse(out)
-        # breakpoint()
 
         return res
