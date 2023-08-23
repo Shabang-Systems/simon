116084a043f9988d41114d330a5fa71d525521ac
Houjun Liu <houjun@jemoka.com>
2023-08-23 09:26:20 -0700

    titling

--- simon/agents/rio.py
+++ simon/agents/rio.py
@@ -42,7 +42,7 @@ John works in Syscorp. [0] Syscorp is an Canadian company with headquarters in S
 
 Each entry in the result must not use more than 7 words, and they must not contain : or ".
 
-You maybe provided resources that are entirely irrelavent. If so, *don't include them!* Use your best judgement to select resources and answers that will help surface unexpected information. Fact chec the resources; if something doesn't make sense, don't include it. 
+*You maybe provided resources that are entirely irrelavent*. If so, *don't include them!* Use your best judgement to select resources and answers that will help surface unexpected information. Fact chec the resources; if something doesn't make sense, don't include it. 
 
 Each entry should be of the EXACT SHAPE:
 
@@ -188,7 +188,7 @@ class RIO(object):
         chunks = {k:v
                   for indx, i in enumerate(kb)
                   for k,v in [(resource_ids[j], indx)
-                              for j in sent_tokenize(i["text"])]}
+                              for j in sent_tokenize(i["metadata"]["title"]+" "+i["text"])]}
 
         # freeze and reverse the resource id dictionary
         # so this is now a dict of resource_id:text
