bbe4b4462fdd7e1bc19c1c4fcbb55fc8028de306
Houjun Liu <houjun@jemoka.com>
2023-08-24 19:20:00 -0700

    adding hash type indexing

--- schema.sql
+++ schema.sql
@@ -39,6 +39,7 @@ CREATE TABLE simon_paragraphs (
 CREATE INDEX simon_paragraphs_embedding_cosine_idx ON simon_paragraphs USING ivfflat (embedding vector_cosine_ops) WITH (lists = 200); 
 CREATE INDEX simon_paragraphs_text_index ON simon_paragraphs USING GIN (text_fuzzy);
 CREATE INDEX simon_paragraphs_chunk_index ON simon_paragraphs USING BTREE (seq);
+CREATE INDEX simon_paragraphs_chunk_hash ON simon_paragraphs USING HASH (hash);
 CREATE INDEX simon_paragraphs_title_index ON simon_paragraphs USING GIN (title_fuzzy);
 CREATE INDEX simon_fulltext_text_index ON simon_fulltext USING GIN (text_fuzzy);
 CREATE INDEX simon_fulltext_title_index ON simon_fulltext USING GIN (title_fuzzy);
