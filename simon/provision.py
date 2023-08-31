48c95fa7b737c5dbe71c2badf79db1fba75bdc16
Houjun Liu <houjun@jemoka.com>
2023-08-30 17:49:19 -0700

    automated provisioning command

--- simon/provision.py
+++ simon/provision.py
@@ -1,7 +1,51 @@
 from psycopg2 import connect
+from psycopg2.errors import DuplicateTable, InFailedSqlTransaction
+
 from .environment import get_db_config
 
 import os
+
+import logging
+L = logging.getLogger("simon")
+
+def __run_setup(cnx):
+    # change working directory to this file (where schema.sql is)
+    workdir = os.getcwd() # save temp dir
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+
+    try:
+        with cnx.cursor() as cursor:
+            cursor.execute(open("schema.sql", "r").read())
+    except (DuplicateTable, InFailedSqlTransaction) as e:
+        cnx.rollback()
+
+        if type(e) == DuplicateTable:
+            raise ValueError("It looks like you have already created the Simon schema for this database.\nYou only have to call this function once per *database*; no need to call this function every time you use Simon on the same database. You should be able to use the rest of Simon's functions without calling this function again.\n\nAs the recreation of Simon's schema will cause data erasure, if you want to recreate the Simon schema from scratch please manually delete and recreate the database with DROP DATABASE [your database] and CREATE DATABASE [your database] within psql, then invoke this function.")
+        else:
+            return __run_setup(cnx)
+    finally:
+        cnx.commit()
+
+        # change workdir back
+        os.chdir(workdir)
+
+def setup(context):
+    """setup simon's necessary tables
+
+    Used for side effects.
+
+    Parameters
+    ----------
+    context : AgentContext
+        The agentcontext to set up the database with.
+    """
+    
+    L.debug("Running Simon setup...") 
+    cnx = context.cnx
+    __run_setup(cnx)
+    L.info("Successfully set up database tables.") 
 
 def execute():
     db_config = get_db_config()
@@ -23,20 +67,7 @@ before continuing.
     print("")
     
     cnx = connect(**db_config)
+    __run_setup(cnx)
 
-    # change working directory to this file (where schema.sql is)
-    workdir = os.getcwd() # save temp dir
-    abspath = os.path.abspath(__file__)
-    dname = os.path.dirname(abspath)
-    os.chdir(dname)
-
-    with cnx.cursor() as cursor:
-        cursor.execute(open("schema.sql", "r").read())
-
-    cnx.commit()
-
-    # change workdir back
-    os.chdir(workdir)
     print("All done now. Happy Simoning!")
     print()
-
